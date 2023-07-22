6a24f2f950364e81943d5bb2cd45c4a3ea0c96d1
dengjie <dengjie@newcapec.net>
2023-07-22 08:32:00 +0800

    feat: 增加 entrypoint 动态监听的测试代码

--- pkg/server/entrypoint_factory_test.go
+++ pkg/server/entrypoint_factory_test.go
@@ -13,9 +13,66 @@ import (
 	"github.com/traefik/traefik/v2/pkg/metrics"
 	"github.com/traefik/traefik/v2/pkg/server/middleware"
 	"github.com/traefik/traefik/v2/pkg/server/service"
-	th "github.com/traefik/traefik/v2/pkg/testhelpers"
 	"github.com/traefik/traefik/v2/pkg/tls"
 )
+
+type entpointTestData struct {
+	name   string
+	conf   dynamic.Configuration
+	tcpNum int
+	udpNum int
+}
+
+func getEntrypointTestData(testServer *httptest.Server) []entpointTestData {
+	data := []entpointTestData{
+		{
+			name:   "initial data 4 tcp, 2 udp",
+			tcpNum: 4,
+			udpNum: 2,
+		}, {
+			name:   "remove 1 tcp => 3 tcp, 2 udp",
+			tcpNum: 3,
+			udpNum: 2,
+		}, {
+			name:   "remove 1 udp => 3 tcp, 1 udp",
+			tcpNum: 3,
+			udpNum: 1,
+		}, {
+			name:   "add 1 tcp => 4 tcp, 1 udp",
+			tcpNum: 4,
+			udpNum: 1,
+		}, {
+			name:   "add 1 tcp => 5 tcp, 1 udp",
+			tcpNum: 5,
+			udpNum: 1,
+		}, {
+			name:   "remove 1 tcp => 4 tcp, 1 udp",
+			tcpNum: 4,
+			udpNum: 1,
+		}, {
+			name:   "add 1 udp => 4 tcp, 2 udp",
+			tcpNum: 4,
+			udpNum: 2,
+		}, {
+			name:   "add 1 duplicated tcp entrypoint => 4 tcp, 2 udp",
+			tcpNum: 4,
+			udpNum: 2,
+		}, {
+			name:   "add 1 duplicated udp entrypoint => 4 tcp, 2 udp",
+			tcpNum: 4,
+			udpNum: 2,
+		}, {
+			name:   "add 1 http router with duplicated entrypoint and a new http router with new entrypoint => 5 tcp, 2 udp",
+			tcpNum: 5,
+			udpNum: 2,
+		},
+	}
+
+	for k, v := range entpointFixtures(testServer.URL) {
+		data[k].conf = v
+	}
+	return data
+}
 
 func Test(t *testing.T) {
 	testServer := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
@@ -26,112 +83,6 @@ func Test(t *testing.T) {
 	staticConfig := static.Configuration{
 		EntryPoints: map[string]*static.EntryPoint{
 			"web": {},
-		},
-	}
-
-	dynamicConfigs1 := dynamic.Configuration{
-		HTTP: th.BuildConfiguration(
-			th.WithRouters(
-				th.WithRouter("foo",
-					th.WithEntryPoints("web"),
-					th.WithServiceName("bar"),
-					th.WithRule("Path(`/ok`)")),
-				th.WithRouter("foo1",
-					th.WithEntryPoints("http-30001"),
-					th.WithRule("Path(`/unauthorized`)"),
-					th.WithServiceName("bar")),
-			),
-			th.WithLoadBalancerServices(th.WithService("bar",
-				th.WithServers(th.WithServer(testServer.URL))),
-			),
-		),
-		TCP: &dynamic.TCPConfiguration{
-			Routers: map[string]*dynamic.TCPRouter{
-				"Router0": {
-					EntryPoints: []string{
-						"tcp-30002",
-					},
-					Service:  "foobar1",
-					Rule:     "foobar",
-					Priority: 42,
-					TLS: &dynamic.RouterTCPTLSConfig{
-						Passthrough: false,
-						Options:     "foo",
-					},
-				},
-				"Router1": {
-					EntryPoints: []string{
-						"tcp-30003",
-					},
-					Service:  "foobar2",
-					Rule:     "foobar",
-					Priority: 42,
-					TLS: &dynamic.RouterTCPTLSConfig{
-						Passthrough: false,
-						Options:     "foo",
-					},
-				},
-			},
-			Services: map[string]*dynamic.TCPService{
-				"foobar1": {
-					LoadBalancer: &dynamic.TCPServersLoadBalancer{
-						Servers: []dynamic.TCPServer{
-							{
-								Port: "42",
-							},
-						},
-						TerminationDelay: func(i int) *int { return &i }(42),
-						ProxyProtocol:    &dynamic.ProxyProtocol{Version: 42},
-					},
-				},
-				"foobar2": {
-					LoadBalancer: &dynamic.TCPServersLoadBalancer{
-						Servers: []dynamic.TCPServer{
-							{
-								Port: "42",
-							},
-						},
-						TerminationDelay: func(i int) *int { return &i }(42),
-						ProxyProtocol:    &dynamic.ProxyProtocol{Version: 42},
-					},
-				},
-			},
-		},
-		UDP: &dynamic.UDPConfiguration{
-			Routers: map[string]*dynamic.UDPRouter{
-				"Router0": {
-					EntryPoints: []string{
-						"udp-30004",
-					},
-					Service: "fiibar1",
-				},
-				"Router1": {
-					EntryPoints: []string{
-						"udp-30005",
-					},
-					Service: "fiibar2",
-				},
-			},
-			Services: map[string]*dynamic.UDPService{
-				"fiibar1": {
-					LoadBalancer: &dynamic.UDPServersLoadBalancer{
-						Servers: []dynamic.UDPServer{
-							{
-								Port: "42",
-							},
-						},
-					},
-				},
-				"fiibar2": {
-					LoadBalancer: &dynamic.UDPServersLoadBalancer{
-						Servers: []dynamic.UDPServer{
-							{
-								Port: "42",
-							},
-						},
-					},
-				},
-			},
 		},
 	}
 
@@ -153,550 +104,13 @@ func Test(t *testing.T) {
 	factory := NewRouterFactory(staticConfig, managerFactory, tlsManager, middleware.NewChainBuilder(nil, nil, nil), nil, metrics.NewVoidRegistry())
 
 	epFactory := NewEntryPointFactory(factory, staticConfig, serverEntryPointsTCP, serverEntryPointsUDP)
-	epFactory.BuildEntryPoints(dynamicConfigs1)
-	assert.Equal(t, 4, len(epFactory.ServerEntryPointsTCP()), "tcp entrypoints length init")
-	assert.Equal(t, 2, len(epFactory.ServerEntryPointsUDP()), "udp entrypoints length init")
-	compareEntryPoints(t, dynamicConfigs1, epFactory)
 
-	dynamicConfigs2 := dynamic.Configuration{
-		HTTP: th.BuildConfiguration(
-			th.WithRouters(
-				th.WithRouter("foo",
-					th.WithEntryPoints("web"),
-					th.WithRule("Path(`/unauthorized`)"),
-					th.WithServiceName("bar")),
-			),
-			th.WithLoadBalancerServices(th.WithService("bar",
-				th.WithServers(th.WithServer(testServer.URL))),
-			),
-		),
-		TCP: &dynamic.TCPConfiguration{
-			Routers: map[string]*dynamic.TCPRouter{
-				"Router0": {
-					EntryPoints: []string{
-						"tcp-30002",
-					},
-					Service:  "foobar1",
-					Rule:     "foobar",
-					Priority: 42,
-					TLS: &dynamic.RouterTCPTLSConfig{
-						Passthrough: false,
-						Options:     "foo",
-					},
-				},
-				"Router1": {
-					EntryPoints: []string{
-						"tcp-30003",
-					},
-					Service:  "foobar2",
-					Rule:     "foobar",
-					Priority: 42,
-					TLS: &dynamic.RouterTCPTLSConfig{
-						Passthrough: false,
-						Options:     "foo",
-					},
-				},
-			},
-			Services: map[string]*dynamic.TCPService{
-				"foobar1": {
-					LoadBalancer: &dynamic.TCPServersLoadBalancer{
-						Servers: []dynamic.TCPServer{
-							{
-								Port: "42",
-							},
-						},
-						TerminationDelay: func(i int) *int { return &i }(42),
-						ProxyProtocol:    &dynamic.ProxyProtocol{Version: 42},
-					},
-				},
-				"foobar2": {
-					LoadBalancer: &dynamic.TCPServersLoadBalancer{
-						Servers: []dynamic.TCPServer{
-							{
-								Port: "42",
-							},
-						},
-						TerminationDelay: func(i int) *int { return &i }(42),
-						ProxyProtocol:    &dynamic.ProxyProtocol{Version: 42},
-					},
-				},
-			},
-		},
-		UDP: &dynamic.UDPConfiguration{
-			Routers: map[string]*dynamic.UDPRouter{
-				"Router0": {
-					EntryPoints: []string{
-						"udp-30004",
-					},
-					Service: "fiibar1",
-				},
-				"Router1": {
-					EntryPoints: []string{
-						"udp-30005",
-					},
-					Service: "fiibar2",
-				},
-			},
-			Services: map[string]*dynamic.UDPService{
-				"fiibar1": {
-					LoadBalancer: &dynamic.UDPServersLoadBalancer{
-						Servers: []dynamic.UDPServer{
-							{
-								Port: "42",
-							},
-						},
-					},
-				},
-				"fiibar2": {
-					LoadBalancer: &dynamic.UDPServersLoadBalancer{
-						Servers: []dynamic.UDPServer{
-							{
-								Port: "42",
-							},
-						},
-					},
-				},
-			},
-		},
+	for _, v := range getEntrypointTestData(testServer) {
+		epFactory.BuildEntryPoints(v.conf)
+		assert.Equal(t, v.tcpNum, len(epFactory.ServerEntryPointsTCP()), "tcp entrypoints length init")
+		assert.Equal(t, v.udpNum, len(epFactory.ServerEntryPointsUDP()), "udp entrypoints length init")
+		compareEntryPoints(t, v.conf, epFactory)
 	}
-	epFactory.BuildEntryPoints(dynamicConfigs2)
-	assert.Equal(t, 3, len(epFactory.ServerEntryPointsTCP()), "tcp entrypoints length remove http router")
-	assert.Equal(t, 2, len(epFactory.ServerEntryPointsUDP()), "udp entrypoints length remove http router")
-	compareEntryPoints(t, dynamicConfigs2, epFactory)
-
-	dynamicConfigs3 := dynamic.Configuration{
-		HTTP: th.BuildConfiguration(
-			th.WithRouters(
-				th.WithRouter("foo",
-					th.WithEntryPoints("web"),
-					th.WithRule("Path(`/unauthorized`)"),
-					th.WithServiceName("bar")),
-			),
-			th.WithLoadBalancerServices(th.WithService("bar",
-				th.WithServers(th.WithServer(testServer.URL))),
-			),
-		),
-		TCP: &dynamic.TCPConfiguration{
-			Routers: map[string]*dynamic.TCPRouter{
-				"Router0": {
-					EntryPoints: []string{
-						"tcp-30002",
-					},
-					Service:  "foobar1",
-					Rule:     "foobar",
-					Priority: 42,
-					TLS: &dynamic.RouterTCPTLSConfig{
-						Passthrough: false,
-						Options:     "foo",
-					},
-				},
-				"Router1": {
-					EntryPoints: []string{
-						"tcp-30003",
-					},
-					Service:  "foobar2",
-					Rule:     "foobar",
-					Priority: 42,
-					TLS: &dynamic.RouterTCPTLSConfig{
-						Passthrough: false,
-						Options:     "foo",
-					},
-				},
-			},
-			Services: map[string]*dynamic.TCPService{
-				"foobar1": {
-					LoadBalancer: &dynamic.TCPServersLoadBalancer{
-						Servers: []dynamic.TCPServer{
-							{
-								Port: "42",
-							},
-						},
-						TerminationDelay: func(i int) *int { return &i }(42),
-						ProxyProtocol:    &dynamic.ProxyProtocol{Version: 42},
-					},
-				},
-				"foobar2": {
-					LoadBalancer: &dynamic.TCPServersLoadBalancer{
-						Servers: []dynamic.TCPServer{
-							{
-								Port: "42",
-							},
-						},
-						TerminationDelay: func(i int) *int { return &i }(42),
-						ProxyProtocol:    &dynamic.ProxyProtocol{Version: 42},
-					},
-				},
-			},
-		},
-		UDP: &dynamic.UDPConfiguration{
-			Routers: map[string]*dynamic.UDPRouter{
-				"Router0": {
-					EntryPoints: []string{
-						"udp-30004",
-					},
-					Service: "fiibar1",
-				},
-			},
-			Services: map[string]*dynamic.UDPService{
-				"fiibar1": {
-					LoadBalancer: &dynamic.UDPServersLoadBalancer{
-						Servers: []dynamic.UDPServer{
-							{
-								Port: "42",
-							},
-						},
-					},
-				},
-				"fiibar2": {
-					LoadBalancer: &dynamic.UDPServersLoadBalancer{
-						Servers: []dynamic.UDPServer{
-							{
-								Port: "42",
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-	epFactory.BuildEntryPoints(dynamicConfigs3)
-	assert.Equal(t, 3, len(epFactory.ServerEntryPointsTCP()), "tcp entrypoints length remove udp")
-	assert.Equal(t, 1, len(epFactory.ServerEntryPointsUDP()), "udp entrypoints length remove udp")
-	compareEntryPoints(t, dynamicConfigs3, epFactory)
-
-	dynamicConfigs4 := dynamic.Configuration{
-		HTTP: th.BuildConfiguration(
-			th.WithRouters(
-				th.WithRouter("foo",
-					th.WithEntryPoints("web"),
-					th.WithRule("Path(`/unauthorized`)"),
-					th.WithServiceName("bar")),
-			),
-			th.WithLoadBalancerServices(th.WithService("bar",
-				th.WithServers(th.WithServer(testServer.URL))),
-			),
-		),
-		TCP: &dynamic.TCPConfiguration{
-			Routers: map[string]*dynamic.TCPRouter{
-				"Router0": {
-					EntryPoints: []string{
-						"tcp-30002",
-					},
-					Service:  "foobar1",
-					Rule:     "foobar",
-					Priority: 42,
-					TLS: &dynamic.RouterTCPTLSConfig{
-						Passthrough: false,
-						Options:     "foo",
-					},
-				},
-				"Router1": {
-					EntryPoints: []string{
-						"tcp-30003",
-					},
-					Service:  "foobar2",
-					Rule:     "foobar",
-					Priority: 42,
-					TLS: &dynamic.RouterTCPTLSConfig{
-						Passthrough: false,
-						Options:     "foo",
-					},
-				},
-				"Router2": {
-					EntryPoints: []string{
-						"tcp-30004",
-					},
-					Service:  "foobar2",
-					Rule:     "foobar",
-					Priority: 42,
-					TLS: &dynamic.RouterTCPTLSConfig{
-						Passthrough: false,
-						Options:     "foo",
-					},
-				},
-			},
-			Services: map[string]*dynamic.TCPService{
-				"foobar1": {
-					LoadBalancer: &dynamic.TCPServersLoadBalancer{
-						Servers: []dynamic.TCPServer{
-							{
-								Port: "42",
-							},
-						},
-						TerminationDelay: func(i int) *int { return &i }(42),
-						ProxyProtocol:    &dynamic.ProxyProtocol{Version: 42},
-					},
-				},
-				"foobar2": {
-					LoadBalancer: &dynamic.TCPServersLoadBalancer{
-						Servers: []dynamic.TCPServer{
-							{
-								Port: "42",
-							},
-						},
-						TerminationDelay: func(i int) *int { return &i }(42),
-						ProxyProtocol:    &dynamic.ProxyProtocol{Version: 42},
-					},
-				},
-			},
-		},
-		UDP: &dynamic.UDPConfiguration{
-			Routers: map[string]*dynamic.UDPRouter{
-				"Router0": {
-					EntryPoints: []string{
-						"udp-30004",
-					},
-					Service: "fiibar1",
-				},
-			},
-			Services: map[string]*dynamic.UDPService{
-				"fiibar1": {
-					LoadBalancer: &dynamic.UDPServersLoadBalancer{
-						Servers: []dynamic.UDPServer{
-							{
-								Port: "42",
-							},
-						},
-					},
-				},
-				"fiibar2": {
-					LoadBalancer: &dynamic.UDPServersLoadBalancer{
-						Servers: []dynamic.UDPServer{
-							{
-								Port: "42",
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-
-	epFactory.BuildEntryPoints(dynamicConfigs4)
-	assert.Equal(t, 4, len(epFactory.ServerEntryPointsTCP()), "tcp entrypoints length add tcp")
-	assert.Equal(t, 1, len(epFactory.ServerEntryPointsUDP()), "udp entrypoints length add tcp")
-	compareEntryPoints(t, dynamicConfigs4, epFactory)
-
-	dynamicConfigs5 := dynamic.Configuration{
-		HTTP: th.BuildConfiguration(
-			th.WithRouters(
-				th.WithRouter("foo",
-					th.WithEntryPoints("web"),
-					th.WithRule("Path(`/unauthorized`)"),
-					th.WithServiceName("bar")),
-				th.WithRouter("foo1",
-					th.WithEntryPoints("http-31001"),
-					th.WithRule("Path(`/unauthorized`)"),
-					th.WithServiceName("bar")),
-			),
-			th.WithLoadBalancerServices(th.WithService("bar",
-				th.WithServers(th.WithServer(testServer.URL))),
-			),
-		),
-		TCP: &dynamic.TCPConfiguration{
-			Routers: map[string]*dynamic.TCPRouter{
-				"Router0": {
-					EntryPoints: []string{
-						"tcp-30002",
-					},
-					Service:  "foobar1",
-					Rule:     "foobar",
-					Priority: 42,
-					TLS: &dynamic.RouterTCPTLSConfig{
-						Passthrough: false,
-						Options:     "foo",
-					},
-				},
-				"Router1": {
-					EntryPoints: []string{
-						"tcp-30003",
-					},
-					Service:  "foobar2",
-					Rule:     "foobar",
-					Priority: 42,
-					TLS: &dynamic.RouterTCPTLSConfig{
-						Passthrough: false,
-						Options:     "foo",
-					},
-				},
-				"Router2": {
-					EntryPoints: []string{
-						"tcp-30004",
-					},
-					Service:  "foobar2",
-					Rule:     "foobar",
-					Priority: 42,
-					TLS: &dynamic.RouterTCPTLSConfig{
-						Passthrough: false,
-						Options:     "foo",
-					},
-				},
-			},
-			Services: map[string]*dynamic.TCPService{
-				"foobar1": {
-					LoadBalancer: &dynamic.TCPServersLoadBalancer{
-						Servers: []dynamic.TCPServer{
-							{
-								Port: "42",
-							},
-						},
-						TerminationDelay: func(i int) *int { return &i }(42),
-						ProxyProtocol:    &dynamic.ProxyProtocol{Version: 42},
-					},
-				},
-				"foobar2": {
-					LoadBalancer: &dynamic.TCPServersLoadBalancer{
-						Servers: []dynamic.TCPServer{
-							{
-								Port: "42",
-							},
-						},
-						TerminationDelay: func(i int) *int { return &i }(42),
-						ProxyProtocol:    &dynamic.ProxyProtocol{Version: 42},
-					},
-				},
-			},
-		},
-		UDP: &dynamic.UDPConfiguration{
-			Routers: map[string]*dynamic.UDPRouter{
-				"Router0": {
-					EntryPoints: []string{
-						"udp-30004",
-					},
-					Service: "fiibar1",
-				},
-			},
-			Services: map[string]*dynamic.UDPService{
-				"fiibar1": {
-					LoadBalancer: &dynamic.UDPServersLoadBalancer{
-						Servers: []dynamic.UDPServer{
-							{
-								Port: "42",
-							},
-						},
-					},
-				},
-				"fiibar2": {
-					LoadBalancer: &dynamic.UDPServersLoadBalancer{
-						Servers: []dynamic.UDPServer{
-							{
-								Port: "42",
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-	epFactory.BuildEntryPoints(dynamicConfigs5)
-	assert.Equal(t, 5, len(epFactory.ServerEntryPointsTCP()), "tcp entrypoints length add http router")
-	assert.Equal(t, 1, len(epFactory.ServerEntryPointsUDP()), "udp entrypoints length add http router")
-	compareEntryPoints(t, dynamicConfigs5, epFactory)
-
-	dynamicConfigs6 := dynamic.Configuration{
-		HTTP: th.BuildConfiguration(
-			th.WithRouters(
-				th.WithRouter("foo",
-					th.WithEntryPoints("web"),
-					th.WithRule("Path(`/unauthorized`)"),
-					th.WithServiceName("bar")),
-				th.WithRouter("foo1",
-					th.WithEntryPoints("http-31001"),
-					th.WithRule("Path(`/unauthorized`)"),
-					th.WithServiceName("bar")),
-			),
-			th.WithLoadBalancerServices(th.WithService("bar",
-				th.WithServers(th.WithServer(testServer.URL))),
-			),
-		),
-		TCP: &dynamic.TCPConfiguration{
-			Routers: map[string]*dynamic.TCPRouter{
-				"Router0": {
-					EntryPoints: []string{
-						"tcp-30002",
-					},
-					Service:  "foobar1",
-					Rule:     "foobar",
-					Priority: 42,
-					TLS: &dynamic.RouterTCPTLSConfig{
-						Passthrough: false,
-						Options:     "foo",
-					},
-				},
-				"Router2": {
-					EntryPoints: []string{
-						"tcp-30004",
-					},
-					Service:  "foobar2",
-					Rule:     "foobar",
-					Priority: 42,
-					TLS: &dynamic.RouterTCPTLSConfig{
-						Passthrough: false,
-						Options:     "foo",
-					},
-				},
-			},
-			Services: map[string]*dynamic.TCPService{
-				"foobar1": {
-					LoadBalancer: &dynamic.TCPServersLoadBalancer{
-						Servers: []dynamic.TCPServer{
-							{
-								Port: "42",
-							},
-						},
-						TerminationDelay: func(i int) *int { return &i }(42),
-						ProxyProtocol:    &dynamic.ProxyProtocol{Version: 42},
-					},
-				},
-				"foobar2": {
-					LoadBalancer: &dynamic.TCPServersLoadBalancer{
-						Servers: []dynamic.TCPServer{
-							{
-								Port: "42",
-							},
-						},
-						TerminationDelay: func(i int) *int { return &i }(42),
-						ProxyProtocol:    &dynamic.ProxyProtocol{Version: 42},
-					},
-				},
-			},
-		},
-		UDP: &dynamic.UDPConfiguration{
-			Routers: map[string]*dynamic.UDPRouter{
-				"Router0": {
-					EntryPoints: []string{
-						"udp-30004",
-					},
-					Service: "fiibar1",
-				},
-			},
-			Services: map[string]*dynamic.UDPService{
-				"fiibar1": {
-					LoadBalancer: &dynamic.UDPServersLoadBalancer{
-						Servers: []dynamic.UDPServer{
-							{
-								Port: "42",
-							},
-						},
-					},
-				},
-				"fiibar2": {
-					LoadBalancer: &dynamic.UDPServersLoadBalancer{
-						Servers: []dynamic.UDPServer{
-							{
-								Port: "42",
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-
-	epFactory.BuildEntryPoints(dynamicConfigs6)
-	assert.Equal(t, 4, len(epFactory.ServerEntryPointsTCP()), "tcp entrypoints length remove tcp again")
-	assert.Equal(t, 1, len(epFactory.ServerEntryPointsUDP()), "udp entrypoints length remove tcp again")
-	compareEntryPoints(t, dynamicConfigs6, epFactory)
 }
 
 func compareEntryPoints(t *testing.T, conf dynamic.Configuration, epFactory *EntryPointFactory) {
